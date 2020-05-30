6803b8dc8994889c16fca7af0f995ec43e3d6a7a
Michael Currin <michael.currin@gmail.com>
2020-05-30 18:55:17 +0200

    feat: Update App.js

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello, world!</h1>
-        <p>
-          GitHub repo:{' '}
-          <a
-            className="App-link"
-            href="https://github.com/MichaelCurrin/react-create-app-quickstart"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            MichaelCurrin/react-create-app-quickstart
-          </a>
-        </p>
+        <p>Welcome to React Create App Quickstart</p>
       </header>
+
+      <p className="App-intro">
+        GitHub repo:{' '}
+        <a
+          className="App-link"
+          href="https://github.com/MichaelCurrin/react-create-app-quickstart"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MichaelCurrin/react-create-app-quickstart
+        </a>
+      </p>
     </div>
   );
 }
