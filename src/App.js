69460ab319b1c50f39613da13a6db000aa0e5803
michaelcurrin <michael.currin@gmail.com>
2020-05-30 18:14:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello, world!
         </a>
       </header>
     </div>
