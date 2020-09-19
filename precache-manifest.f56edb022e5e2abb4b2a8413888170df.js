f47a2d2eca096cd78276cb54555770c605b33def
MichaelCurrin <MichaelCurrin@users.noreply.github.com>
2020-09-19 14:40:31 +0000

    deploy: bfbdce7b377e2a941492e7add8707ff2b3902525

---
+++ precache-manifest.f56edb022e5e2abb4b2a8413888170df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531d43a91206893ef89805b7edbc250c",
+    "url": "/react-quickstart/index.html"
+  },
+  {
+    "revision": "bfb4f8ccde8b101aa7ec",
+    "url": "/react-quickstart/static/css/main.13ad298f.chunk.css"
+  },
+  {
+    "revision": "7f40cb5a059d169f88a5",
+    "url": "/react-quickstart/static/js/2.796b80d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quickstart/static/js/2.796b80d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb4f8ccde8b101aa7ec",
+    "url": "/react-quickstart/static/js/main.d7d728be.chunk.js"
+  },
+  {
+    "revision": "30a37d2525b37d87d281",
+    "url": "/react-quickstart/static/js/runtime-main.712406c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-quickstart/static/media/logo.5d5d9eef.svg"
+  }
+]);
