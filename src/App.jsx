4cf939c795fe2cf56e65dc1dfbbdb4ec6e8d8e14
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-11-16 16:04:15 +0200

    style: White space changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,31 @@ import Foo from './components/Foo';
 import logo from './logo.svg';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
 
-				<h1>Hello, world!</h1>
-				<p>Welcome to React Quickstart</p>
-			</header>
+        <h1>Hello, world!</h1>
+        <p>Welcome to React Quickstart</p>
+      </header>
 
-			<div>
-				<p>
-					Using the <code>Foo</code> component:
+      <div>
+        <p>
+          Using the <code>Foo</code> component:
 				</p>
-				<Foo text="Dynamic text" />
-				<br />
+        <Foo text="Dynamic text" />
+        <br />
 
-				<p>
-					GitHub repo:{' '}
-					<a href="https://github.com/MichaelCurrin/react-quickstart" target="_blank" rel="noopener noreferrer">
-						MichaelCurrin/react-quickstart
+        <p>
+          GitHub repo:{' '}
+          <a href="https://github.com/MichaelCurrin/react-quickstart" target="_blank" rel="noopener noreferrer">
+            MichaelCurrin/react-quickstart
 					</a>
-				</p>
-			</div>
-		</div>
-	);
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
