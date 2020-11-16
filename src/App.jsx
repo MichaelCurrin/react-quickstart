63ded2ed52618da16a319201bd0ab1790f79c5e5
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-11-16 14:48:26 +0200

    feat: Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
-import React from 'react';
-
 import './App.css';
-import logo from './logo.svg';
 import Foo from './components/Foo';
+import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-    
-        <h1>Hello, world!</h1>
-        <p>Welcome to React Quickstart</p>
-      </header>
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
 
-      <div>
-        <p>Using the <code>Foo</code> component:</p>
-        <Foo text="Dynamic text" />
-        <br />
+				<h1>Hello, world!</h1>
+				<p>Welcome to React Quickstart</p>
+			</header>
 
-        <p>
-          GitHub repo:{' '}
-          <a
-            href="https://github.com/MichaelCurrin/react-quickstart"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            MichaelCurrin/react-quickstart
-          </a>
-        </p>
-      </div>
-    </div>
-  );
+			<div>
+				<p>
+					Using the <code>Foo</code> component:
+				</p>
+				<Foo text="Dynamic text" />
+				<br />
+
+				<p>
+					GitHub repo:{' '}
+					<a href="https://github.com/MichaelCurrin/react-quickstart" target="_blank" rel="noopener noreferrer">
+						MichaelCurrin/react-quickstart
+					</a>
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
