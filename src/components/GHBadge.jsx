55d3922ba2ba8a013a6045b6fde6c85fc808d898
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-21 15:59:52 +0200

    fix

--- src/components/GHBadge.jsx
+++ src/components/GHBadge.jsx
@@ -3,7 +3,7 @@ export default function GHBadge(props) {
 
   const repoUrl = `https://github.com/${username}/${repoName}`;
   const badgeUrl = `https://img.shields.io/static/v1?label=${username}&message=${repoName}&color=blue&logo=github`;
-  const altText = `${username} - repoName`;
+  const altText = `${username} - ${repoName}`;
 
   return (
     <a href={repoUrl}>
