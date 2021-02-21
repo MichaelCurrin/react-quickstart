fc6fa5964960a4420ae1104b125560f7d5799a87
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-21 13:38:41 +0200

    feat: Add shield

--- src/components/GHBadge.jsx
+++ src/components/GHBadge.jsx
@@ -2,13 +2,12 @@ export default function GHBadge(props) {
   const { username, repoName } = props;
 
   const repoUrl = `https://github.com/${username}/${repoName}`;
+  const badgeUrl = `https://img.shields.io/static/v1?label=${username}&message=${repoName}&color=blue&logo=github`;
+  const altText = `${username} - repoName`;
 
   return (
-    <p>
-      GitHub repo:{" "}
-      <a href={repoUrl} target="_blank" rel="noopener noreferrer">
-        {username}/{repoName}
-      </a>
-    </p>
+    <a href={repoUrl}>
+      <img src={badgeUrl} alt={altText} />
+    </a>
   );
 }
