db0c102743961646e6667dc54d22c936b70e24c8
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-12-28 21:24:12 +0200

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
       <main>
         <Counter />
 
-        <Toggle text="Dynamic text" />
+        <Toggle text="Make me disappear" />
 
         <Parent />
       </main>
