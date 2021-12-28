a69f36f0e3894ddf78e6b9d90e9f50afd30c8279
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-12-28 21:24:23 +0200

    update Child.jsx

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -3,11 +3,11 @@ export default function Child({ value, setValue }) {
     <div className="Child">
       <h3>Child component</h3>
 
-      <p>Value: {value}</p>
-
       <div>
         <button onClick={() => setValue(456)}>Set to 456</button>
       </div>
+
+      <p>Value: {value}</p>
     </div>
   );
 }
