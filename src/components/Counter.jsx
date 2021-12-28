f1eb880afe95b668947d985423a9c3c84b137297
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-12-28 21:24:29 +0200

    update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,7 @@ export default function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <>
       <h2>Counter component</h2>
 
       <p>A simple functional component.</p>
@@ -20,6 +20,6 @@ export default function Counter() {
       <p>You clicked {count} times</p>
 
       <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
+    </>
   );
 }
